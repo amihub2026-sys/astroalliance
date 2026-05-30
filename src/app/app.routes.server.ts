1d915524b4d12d40af7032a0de37def5ef7b8644
amihub <amihub2026@gmail.com>
2026-05-30 12:26:32 +0530

    fix SSR profile print route

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,15 +1,16 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-
   {
     path: 'admin/profile/:id',
     renderMode: RenderMode.Server
   },
-
+  {
+    path: 'admin/profile-print/:id',
+    renderMode: RenderMode.Server
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
   }
-
 ];
